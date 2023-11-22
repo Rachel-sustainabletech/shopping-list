36f6dcb750768b2c32cf729f17f0e7c8755e6b56
Rachel-sustainabletech <rachel.mathilda@ui.ac.id>
2023-11-22 13:32:54 +0700

    selesai

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'shopping_list.urls'
